e95fae145d1ad9f74a602fc3fe1a60f969347d8e
Lilly <lilly@lillycat.tech>
2022-06-20 22:11:14 +0100

    chore: update dna

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -4,5 +4,12 @@ import Data.Map (Map)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
+isNucleotide xs = case x of
+										'G' -> True
+                		'C' -> True
+                		'T' -> True
+                		'A' -> True
+                		 _  -> False
+
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+nucleotideCounts xs = 
